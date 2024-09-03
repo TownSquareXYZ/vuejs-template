4745ff6bdac176740f8ee31d5ceb621a297dd300
cici090 <cici00099000@gmail.com>
2024-09-04 00:44:51 +0800

    add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,32 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  server: {
-    host: true,
-    port: 8000,
-  },
-  optimizeDeps: {
-    include: ['vue','buffer'],
-  },
-  plugins: [
-    vue(),
-    // basicSsl()
-  ],
-  resolve: {
-    alias: {
-      'vue': 'vue/dist/vue.esm-bundler.js',
+export default ({ mode }) => {
+  const isGitHubPages = loadEnv(mode, process.cwd()).VITE_GH_PAGES == "1";
+  console.log(isGitHubPages);
+  return defineConfig({
+    base: isGitHubPages ? '/vuejs-template/' : './',
+    server: {
+      host: true,
+      port: 8000,
     },
-  },
-})
+    optimizeDeps: {
+      include: ['vue', 'buffer'],
+    },
+    build: {
+      outDir: 'docs',
+    },
+
+    plugins: [
+      vue(),
+      // basicSsl()
+    ],
+    resolve: {
+      alias: {
+        'vue': 'vue/dist/vue.esm-bundler.js',
+      },
+    },
+  })
+}
